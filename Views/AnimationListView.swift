29f410a41df4dddc471de0f819ee7376cc9d09ab
Eason Ong <eong@lcs.on.ca>
2024-01-29 08:52:00 -0500

    Organized project and created a list view to show interesting animations.

---
+++ Views/AnimationListView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationListView.swift
+//  LottieAnimationsList
+//
+//  Created by Ong Eason on 29/1/2024.
+//
+
+import SwiftUI
+
+struct AnimationListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationListView()
+}
